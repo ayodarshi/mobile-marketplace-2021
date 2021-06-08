d3aa76ddc61d3404f605f1ebab9481dd02d90d22
buddhi-1997 <buddhi3355@gmail.com>
2021-06-08 16:55:07 +0530

    my listing page done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,21 @@
 export const environment = {
   production: true,
-  
+
   firebaseConfig :{
+    
     apiKey: "AIzaSyDUjAPjK3gU8kugS3Wt8JTxQcOC0vnwiG8",
     authDomain: "mobile-marketplace-77f19.firebaseapp.com",
+    databaseURL: "https://mobile-marketplace-77f19-default-rtdb.firebaseio.com",
     projectId: "mobile-marketplace-77f19",
     storageBucket: "mobile-marketplace-77f19.appspot.com",
     messagingSenderId: "481835323857",
     appId: "1:481835323857:web:826a878bbe1e23123310f0"
+
+    /*apiKey: "AIzaSyA3Y8mFrJgGgN7JIiXtf9Q3WBVKUlVz5zM",
+    authDomain: "mobile-marketplace1.firebaseapp.com",
+    projectId: "mobile-marketplace1",
+    storageBucket: "mobile-marketplace1.appspot.com",
+    messagingSenderId: "490632260310",
+    appId: "1:490632260310:web:4e7bc8e1f71596fdf37777",*/
   }
 };
