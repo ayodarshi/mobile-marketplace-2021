c0ff2342033cb91bcb8420dfa1bae4e28d09d55e
ayodarshi <ayomidarshika12@gmail.com>
2021-06-08 22:21:09 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
 
-  firebaseConfig :{
+  /*firebaseConfig :{
     
     apiKey: "AIzaSyDUjAPjK3gU8kugS3Wt8JTxQcOC0vnwiG8",
     authDomain: "mobile-marketplace-77f19.firebaseapp.com",
@@ -11,11 +11,11 @@ export const environment = {
     messagingSenderId: "481835323857",
     appId: "1:481835323857:web:826a878bbe1e23123310f0"
 
-    /*apiKey: "AIzaSyA3Y8mFrJgGgN7JIiXtf9Q3WBVKUlVz5zM",
+    apiKey: "AIzaSyA3Y8mFrJgGgN7JIiXtf9Q3WBVKUlVz5zM",
     authDomain: "mobile-marketplace1.firebaseapp.com",
     projectId: "mobile-marketplace1",
     storageBucket: "mobile-marketplace1.appspot.com",
     messagingSenderId: "490632260310",
     appId: "1:490632260310:web:4e7bc8e1f71596fdf37777",*/
-  }
+  
 };
